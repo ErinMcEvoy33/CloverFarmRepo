4a9671f3877358d66d12d4a22289d020b7d52538
ErinMcEvoy33 <erinmcevoy33@gmail.com>
2025-12-04 17:26:49 +0000

    Update Michael.js
    updated script file

--- MScript/Michael.js
+++ MScript/Michael.js
@@ -1,24 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("bookingform");
-
+ 
   form.onsubmit = function (e) {
     e.preventDefault();
-
+ 
     const name = document.getElementById("name").value; //get values of what was inputted in the form
     const email = document.getElementById("email").value;
     const event = document.getElementById("Event").value;
-
+ 
     alert(`Thanks, ${name}! You've booked the ${event}  event`); //booking confirmation message
   };
-
+ 
 });
-
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("bookingform1");
+ 
+  form.onsubmit = function (e) {
+    e.preventDefault();
+ 
+    alert(`Thanks, we will be in touch`); //booking confirmation message
+  };
+ 
+});
+ 
 // Name validation
     if (name.value.trim() === "") {
       nameField.classList.add("input-error");
       isValid = false;
     }
-
+ 
 function runQuiz(){ //function to run the weekly quiz
   var questions = [ //array to store the prompts and the answers
   {
@@ -42,8 +52,8 @@ function runQuiz(){ //function to run the weekly quiz
     answer:"a"
   },
 ];
-
-
+ 
+ 
     var score = 0; //calculate the score
     for (var i = 0; i < questions.length; i++){ //loop through the questions
       var response = window.prompt(questions[i].prompt) //get the response and store it 
@@ -56,5 +66,5 @@ function runQuiz(){ //function to run the weekly quiz
       }
     }
     alert("you got " + score + " right"); // output the score
-
+ 
 }
